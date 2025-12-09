eca6c10f345271e6993a6b167ade862ae22bce1c
Hyeonuk Bu <chillysaucing@gmail.com>
2025-12-09 11:54:20 +0900

    prefetch 테스트

--- components/movie-info.tsx
+++ components/movie-info.tsx
@@ -2,7 +2,7 @@ import { API_URL } from "../app/(home)/page";
 import styles from "../styles/movie-info.module.css";
 
 export async function getMovie(movieId: string) {
-  // await new Promise((resolve) => setTimeout(resolve, 5000));
+  await new Promise((resolve) => setTimeout(resolve, 5000));
   console.log(`Fetching movies: ${Date.now()}`);
   const response = await fetch(`${API_URL}/${movieId}`, {
     cache: "force-cache",
