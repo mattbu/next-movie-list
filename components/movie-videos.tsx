8a823a6c86650b5f0320369ffc459e01d9194b92
mattbu <chillysaucing@gmail.com>
2025-12-09 10:25:23 +0900

    movie-detail 페이지 데이터 바인딩, 스타일

--- components/movie-videos.tsx
+++ components/movie-videos.tsx
@@ -1,19 +1,30 @@
 import { API_URL } from "../app/(home)/page";
+import styles from "../styles/movie-videos.module.css";
 
 async function getVideos(movieId: string) {
   // await new Promise((resolve) => setTimeout(resolve, 3000));
-  console.log(`Fetching videos: ${Date.now()}`);
+  // console.log(`Fetching videos: ${Date.now()}`);
 
-  await new Promise((resolve) => setTimeout(resolve, 3000));
-  throw new Error("something broke....");
-  // const response = await fetch(`${API_URL}/${movieId}/videos`, {
-  //   cache: "force-cache",
-  // });
-  // const json = await response.json();
-  // return json;
+  const response = await fetch(`${API_URL}/${movieId}/videos`, {
+    cache: "force-cache",
+  });
+  const json = await response.json();
+  return json;
 }
 
 export default async function MovieVideos({ id }: { id: string }) {
   const videos = await getVideos(id);
-  return <h6>{JSON.stringify(videos)}</h6>;
+  return (
+    <div className={styles.container}>
+      {videos.map((video) => (
+        <iframe
+          key={video.id}
+          src={`https://youtube.com/embed/${video.key}`}
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowFullScreen
+          title={video.name}
+        />
+      ))}
+    </div>
+  );
 }
